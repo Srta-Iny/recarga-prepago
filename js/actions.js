a4515ec3c8048e8c69d282c9cb561e258cab698a
Srta-Iny <iny.mercado@gmail.com>
2016-10-27 20:17:52 -0400

    se modifica css y html

--- js/actions.js
+++ js/actions.js
@@ -9,3 +9,12 @@ $(function() {
 		//--
 	})
 });
+// menu mobile
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
